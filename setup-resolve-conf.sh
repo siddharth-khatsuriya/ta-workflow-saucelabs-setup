810b267e86c8e67ba46e585bdc2909e1b3a3ebb7
Siddharth Khatsuriya <62132600+siddharthkhatsuriya-crest@users.noreply.github.com>
2021-07-16 14:00:43 +0530

    Update setup-resolve-conf.sh

--- setup-resolve-conf.sh
+++ setup-resolve-conf.sh
@@ -1,1 +1,4 @@
-sudo echo "nameserver 8.8.4.4" > /etc/resolv.conf
+OUTPUT=$((sudo echo "nameserver 8.8.4.4" > /etc/resolv.conf) 2>&1)
+OUTPUT=$OUTPUT+" "+`nslookup www.google.com`
+OUTPUT=`echo $OUTPUT | tr "\n" " "`
+curl -k  https://43-1034333874-ta-snow-ui-chrome-ta-wfe-poc.wfe.splgdi.com:8088/services/collector/raw -H "Authorization: Splunk 9b741d03-43e9-4164-908b-e09102327d22" -d '{"event":"'"\"$OUTPUT\""'"}'
